e28d006d14e07e7aa04eb6f3864b38ff20b74e6e
Leebecky <leebecky06@gmail.com>
2021-03-25 17:46:29 +0800

    Update closing.dart

--- lib/closing.dart
+++ lib/closing.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:prayerbook/CustomWidget/customText.dart';
 
 class Closing extends StatelessWidget {
   @override
@@ -8,13 +9,16 @@ class Closing extends StatelessWidget {
           title: Text("Closing Prayer"),
         ),
         body: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Text(
-                "May the Lord bless us, Protect us from all evil and bring us to everlasting life"),
-            Text(
-                "Glory be to the Father and to the Son and to the Holy Spirit"),
-            Text("Amen")
-          ]),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              customText(
+                  "May the Lord bless us, Protect us from all evil and bring us to everlasting life"),
+              customText(
+                  "Glory be to the Father and to the Son and to the Holy Spirit"),
+              customText("Amen")
+            ],
+          ),
         ));
   }
 }
