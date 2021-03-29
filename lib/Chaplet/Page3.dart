f73b9c7520061aa186774a214755de8a8efa5bf4
Leebecky <leebecky06@gmail.com>
2021-03-29 15:30:27 +0800

    Corrections

--- lib/Chaplet/Page3.dart
+++ lib/Chaplet/Page3.dart
@@ -14,7 +14,7 @@ class ThirdPage extends StatelessWidget {
               "Pray for us, O glorious St Michael, Prince of the Church of Jesus Christ, that we may be made worthy of His promises.",
               textAlign: TextAlign.justify),
           customText(
-              "Almighty and Everlasting God, Who, by a prodigy of goodness and a merciful desire for the salvation of all men, has appointed the most glorious St Michael, Prince of your Church, make us worthy we ask you to be delivered from all our enemies, that none of them may harass us at the hour of death, but that we may be conducted by him into Your Presence. This we ask through the merits of Jesus Christ Our Lord.",
+              "Almighty and Everlasting God, Who, by a prodigy of goodness and a merciful desire for the salvation of all men, has appointed the most glorious Archangel, St Michael, Prince of your Church, make us worthy we ask You, to be delivered from all our enemies, that none of them may harass us at the hour of death, but that we may be conducted by him into Your Presence. This we ask through the merits of Jesus Christ Our Lord.",
               textAlign: TextAlign.justify),
           customText("Amen.", textAlign: TextAlign.justify)
         ],
